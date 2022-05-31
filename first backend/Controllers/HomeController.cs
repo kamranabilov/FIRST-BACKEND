78782a2a474ddb4cad1c5b719bbce55139520680
kamranabilov <kamrangab@code.edu.az>
2022-06-01 00:43:19 +0400

    added task

---
+++ first backend/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace first_backend.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ViewResult Index()
+        {
+            ViewResult view = new ViewResult();
+            view.ViewName = "HomePage";
+            return view;
+        }
+
+        public ViewResult About()
+        {
+            return new ViewResult { ViewName = "About" };
+        }
+
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+        
+
+    }
+}
---
+++ first backend/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace first_backend.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Detail(int id)
+
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            //return Content(id.ToString());
+            return View();
+        }
+}    }
